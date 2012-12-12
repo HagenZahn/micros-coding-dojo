be1cfa53b5194e6847a54c7bdf826094b681c700
HagenZahn <hagen.zahn@micros.com>
2012-12-12 16:05:43 +0100

    add termin3 gruppe2

---
+++ archive/termin3/gruppe2/FizzBuzz.groovy
@@ -0,0 +1,24 @@
+
+class FizzBuzz {
+
+    def rules = []
+
+    FizzBuzz() {
+        addRule( [{it % 3 == 0 && it % 5 == 0}, "FizzBuzz"] )
+        addRule( [{it % 3 == 0}, "Fizz"] )
+        addRule( [{it % 5 == 0}, "Buzz"] )
+    }
+
+    def addRule(Closure rule, def result) {
+        rules.add( [rule, result] )
+    }
+
+    def getNumbers() {
+        def numbers = 1..100
+        numbers.collect {
+            def findResult = rules.find{rule -> rule[0](it)}
+            findResult == null ? it : findResult[1]
+        }
+    }
+
+}
